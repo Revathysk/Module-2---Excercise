131c40fd1dfa496604b57660d09d3011b50f32d3
Revathy <revathyskandasamy@gmail.com>
2022-01-12 22:56:02 -0600

    added form

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-
+//     item: 'Milk',
+//     brand: 'Organic Valley',
+//     units: '1 Gallon',
+//     quantity: 0,
+//     isPurchased: false
 
 class App extends React.Component {
 
     state = {
         gList: groceryList,
-        listitems:['Milk']
+        item:'',
+        brand:'',
+        quantity:'1',        
+        PurchaseItems:[]
+    }
+    
+    handleChange=(event)=>{
+        this.setState({
+            [event.target.id]: event.target.value            
+        })
+    }
+
+    handleAdd=(event)=>{
+        
+        console.log('in handleAdd to list',this.state.item)       
+        const newItem = {
+            item: this.state.item,
+            brand: this.state.brand,
+            units: '1',
+            quantity: this.state.quantity,
+            isPurchased:false
+        }
+
+         this.setState({
+             gList: [newItem,...this.state.gList],
+             item:'',
+             brand:'',
+             quantity:'1',
+         } )       
     }
 
     render(){
@@ -13,26 +45,38 @@ class App extends React.Component {
         return(            
             <div class ="main-div">
                 
-                <div class="itemslist">
+                <div id="additems">
+                    <form>
+                        <label htmlFor="Itemname"> Item Name</label>
+                        <input id="itemname" type="text" value={this.state.item} onChange={this.handleChange}/>
+                        <label htmlFor="Itembrand"> Item Brand</label>
+                        <input id="brand" type="text" value={this.state.brand} onChange={this.handleChange}/>
+                        <label htmlFor="quantity"> Quantity </label>
+                        <input id="quantity" type="number" value={this.state.quantity} onChange={this.handleChange}/>
+                    </form>
+
+                    <button id='addtolist' onClick={this.handleAdd}> Add To List</button>
+                  
+                </div>
+
+                <div class="grocerylist">
                     <h1> Template List </h1>  
 
                     <ul>
-                        { this.state.gList.map((gitems)=>
-                        <li> {gitems.item} - {gitems.brand} - {gitems.quantity} </li> )
+                        { this.state.gList.map((grocerylist) =>                        
+                        <li> {grocerylist.item} - {grocerylist.brand} - {grocerylist.quantity} </li> )
                         } 
                     </ul>
-                    <button id='addTemplate' onClick={handleAddList()}> Add to GroceryList</button>
-                    <button id='addPurchase'> Add to Purchase</button>
+                </div>
+                
+                <div class="purchaselist">
+                    <h1> Purchase List </h1>                               
+                    <ul>                         
+                    </ul>
+
+                    <button id='Remove '> Remove </button>
                 </div>
 
-                <div class="topurchase">
-                    <h1> Purchase List </h1>                               
-            
-                    <ul>
-                        {this.state.listitems}
-                    </ul>
-                    <button id='removepurchase'> Remove </button>
-                </div>
             </div>
         )
     }
